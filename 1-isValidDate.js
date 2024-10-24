e113cd9612bf50f9601115dc66bec098e716278b
benspector-mls <ben@marcylabschool.org>
2024-10-24 11:00:13 -0400

    fixing date

--- 1-isValidDate.js
+++ 1-isValidDate.js
@@ -1,9 +1,11 @@
 const isValidDateStr = (dateStr) => {
   // requires 'MM-DD-YYYY' formatted date strings
-  const dateRegExp = /^([01]{2}|12)-([0-2][0-9]|30|31)-[0-9]{4}$/;
+  const dateRegExp = /^(0[1-9]|1[0-2])-([0-2][0-9]|30|31)-[0-9]{4}$/;
   return dateRegExp.test(dateStr);
 }
+// 00 01 02 03 04 05 06 07 08 09
 
+console.log(isValidDateStr('09-12-1995'));
 console.log(isValidDateStr('10-12-1995'));    // true
 console.log(isValidDateStr('10-12-9999'));    // true
 console.log(isValidDateStr('13-12-9999'));    // false
